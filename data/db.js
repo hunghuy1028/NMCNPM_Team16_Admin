ef253d081b498a023c4515dd35c97e44b057072d
VinhNguyen <nhvinh604@gmail.com>
2020-01-02 10:35:41 +0700

    models added

---
+++ data/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://admin123:admin123@cluster0-zdvf1.mongodb.net/test?retryWrites=true&w=majority', 
+{
+    useNewUrlParser: true, 
+    useCreateIndex: true, 
+    useUnifiedTopology: true 
+}).then (
+    () => {
+        console.log("DB connected!");
+    },
+    err => {
+        console.log(err);
+        console.log("DB failed to connect!");
+    }
+);
+
+module.exports = mongoose;
